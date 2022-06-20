6a37c59e2b99a8e2ccb8e4ddd0f33bfcbb6e474c
Taj K <taj.kirkpatrick@gmail.com>
2022-06-20 17:08:59 -0500

    mocking practice and failing tests

--- src/app.controller.spec.ts
+++ src/app.controller.spec.ts
@@ -3,14 +3,22 @@ import { vi } from 'vitest';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 
+const mockAppService = {
+  getHello: vi.fn(() => 'Hello Todd!'),
+  goodbye: vi.fn(() => 'Goodbye Test Master!'),
+};
+
 describe('AppController', async () => {
   const app: TestingModule = await Test.createTestingModule({
     controllers: [AppController],
     providers: [AppService],
-  }).compile();
+  })
+    .overrideProvider(AppService)
+    .useValue(mockAppService)
+    .compile();
 
   const appService = app.get<AppService>(AppService);
-  const appController = app.get(AppController);
+  const appController = app.get<AppController>(AppController);
 
   beforeEach(async () => {
     await app.init();
@@ -24,19 +32,18 @@ describe('AppController', async () => {
     expect(app).toBeDefined();
   });
 
-  it('should be an instance of TestingModule', () => {
-    expect(app).toBeInstanceOf(TestingModule);
-  });
-
-  it('should be type of object', () => {
-    expect(app).toBeTypeOf('object');
-  });
-
-  it('appController should return Hello World', () => {
+  it('should return Hello World', () => {
     vi.spyOn(appController, 'getHello').mockImplementationOnce(() =>
       appService.getHello(),
     );
 
     expect(appController.getHello()).toBe('Hello World!');
+  });
+
+  it('should return Goodbye World', () => {
+    vi.spyOn(appController, 'getGoodbye').mockImplementation(() =>
+      appService.goodbye(),
+    );
+    expect(appController.getGoodbye()).toBe('Goodbye World!');
   });
 });
--- src/app.controller.ts
+++ src/app.controller.ts
@@ -9,4 +9,9 @@ export class AppController {
   getHello(): string {
     return this.appService.getHello();
   }
+
+  @Get()
+  getGoodbye(): string {
+    return this.appService.goodbye();
+  }
 }
