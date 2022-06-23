88b6eb94ee9e53d12baafbcb3e6d800ed49e11cb
Taj K <taj.kirkpatrick@gmail.com>
2022-06-23 17:51:48 -0500

    abandon msw support for now

--- tests/todo.e2e-spec.ts
+++ tests/todo.e2e-spec.ts
@@ -3,33 +3,7 @@ import { Test, TestingModule } from '@nestjs/testing';
 import { AppModule } from './../src/app.module';
 import request = require('supertest');
 
-import { rest } from 'msw';
-import { setupServer } from 'msw/node';
-
-describe('MSW Setup (e2e)', () => {
-  const restHandlers = [
-    rest.get('/health/check', (req, res, ctx) => {
-      return res(ctx.status(200), ctx.json({ status: 'ok' }));
-    }),
-  ];
-  const server = setupServer(...restHandlers);
-
-  //   Nest JS stuff
-
-  beforeAll(async () => {
-    server.listen({ onUnhandledRequest: 'error' });
-  });
-
-  afterEach(() => server.resetHandlers());
-
-  afterAll(() => server.close());
-
-  it('msw is initialized', () => {
-    expect(server).toBeDefined();
-  });
-});
-
-describe('App-e2e-test', async () => {
+describe('Todo (e2e)', async () => {
   let app: INestApplication;
 
   beforeEach(async () => {
@@ -45,10 +19,8 @@ describe('App-e2e-test', async () => {
     await app.close();
   });
 
-  describe('predefined server and app', () => {
-    it('should be defined', () => {
-      expect(app).toBeDefined();
-    });
+  it('app should be defined', () => {
+    expect(app).toBeDefined();
   });
 
   it(`GET /`, async () => {
@@ -62,15 +34,5 @@ describe('App-e2e-test', async () => {
 
     expect(response.statusCode).toBe(200);
     expect(response.text).toBe('Goodbye World!');
-  });
-
-  it('should hit the msw endpoint', async () => {
-    const response: request.Response = await request(app.getHttpServer()).get(
-      '/health/check',
-    );
-
-    console.log(response);
-
-    expect(response.statusCode).toBe(200);
   });
 });
